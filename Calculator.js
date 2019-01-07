21d36aa76373f80c5238da7bc0b0fb20d8446c6f
Tim Ryding <tryding2@gmail.com>
2019-01-07 15:51:42 -0500

    test

---
+++ Calculator.js
@@ -0,0 +1,38 @@
+const calculator = {
+    returnValue: 0,
+    addition: function(num1, num2 = this.returnValue) 
+    {
+        this.returnValue = num1 + num2;
+        return this.returnValue
+    },
+    subtraction: function(num1, num2 = this.returnValue) 
+    {
+        this.returnValue = num2 - num1
+        return this.returnValue
+    },
+    multiplication: function(num1, num2 = this.returnValue) 
+    {
+        this.returnValue = num1 * num2
+        return this.returnValue
+    },
+    division: function(num1, num2 = this.returnValue) 
+    {
+        this.returnValue = num2 / num1
+        return this.returnValue
+    },
+    clearCalc: function() {
+        this.returnValue = 0
+        return this.returnValue
+    }
+}
+
+console.log(calculator.addition(5+5))
+
+console.log(calculator.subtraction(1))
+
+console.log(calculator.division(3))
+
+console.log(calculator.multiplication(25))
+
+console.log(calculator.clearCalc())
+
